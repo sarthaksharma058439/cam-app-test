335179c33d162eab5aedba6d3d19bc2e434655ce
sarthaksharma058439 <48406976+sarthaksharma058439@users.noreply.github.com>
2022-04-05 10:07:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
-app = Flask(__name__)
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+import pyscreenshot
+import flask
+from io import BytesIO
+
+app = flask.Flask(__name__)
+
+@app.route('/screen.png')
+def serve_pil_image():
+    img_io = BytesIO()
+    pyscreenshot.grab().save(img_io, 'PNG', quality=50)
+    img_io.seek(0)
+    return flask.send_file(img_io, mimetype='image/png')
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def serve_img():
+    return flask.render_template('index.html')
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host= '0.0.0.0', debug=True)
